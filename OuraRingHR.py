69c79a91fe336044a26757c50c349a2ba31d1457
Tom Moore <thommoore@chapman.edu>
2025-06-10 18:29:52 -0700

    Added some stuff for converting to date

--- OuraRingHR.py
+++ OuraRingHR.py
@@ -13,6 +13,13 @@ def convert_string_to_time(time_string):
     time_obj = datetime.strptime(time_string, "%H:%M:%S").time()
     return time_obj
 
+def convert_iso_to_pacific_date(timestamp):
+    pacific_tz = pytz.timezone('America/Los_Angeles')
+    dt_utc = datetime.strptime(timestamp, "%Y-%m-%dT%H:%M:%S.%fZ")
+    dt_utc = dt_utc.replace(tzinfo=timezone.utc)
+    dt_pacific = dt_utc.astimezone(pacific_tz)
+    return dt_pacific.date()
+
 rawDataPath = input("Enter the file path of the raw data: ")
 rawData = pd.read_csv(rawDataPath)
 
@@ -22,3 +29,5 @@ scheduleData = pd.read_csv(scheduleDataPath)
 zero_time = datetime(1900, 1, 1, 0, 0, 0).time()
 rawData.insert(0, 'class', "NONE")
 rawData.insert(1, 'Time_In_PST', zero_time)
+
+nextDate = datetime(1900, 1, 1, 0, 0, 0).date()
