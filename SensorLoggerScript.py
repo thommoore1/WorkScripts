5257f4fad962008f2745a6d907aed614b293327d
Tom Moore <thommoore@chapman.edu>
2025-06-12 17:17:15 -0700

    Updated for easier file reading

--- SensorLoggerScript.py
+++ SensorLoggerScript.py
@@ -12,27 +12,27 @@ def convert_string_to_time(time_string):
     time_obj = datetime.strptime(time_string, "%H:%M:%S").time()
     return time_obj
 
-rawDataPath = input("Enter the file path of the raw data: ")
-rawData = pd.read_csv(rawDataPath)
+# rawDataPath = input("Enter the file path of the raw data: ")
+# rawData = pd.read_csv(rawDataPath)
 
-scheduleDataPath = input("Enter the file path of the schedule data: ")
-scheduleData = pd.read_csv(scheduleDataPath)
+# scheduleDataPath = input("Enter the file path of the schedule data: ")
+# scheduleData = pd.read_csv(scheduleDataPath)
 
-saveLocation = input("Enter file path of where you would like to save to: ")
+# saveLocation = input("Enter file path of where you would like to save to: ")
 
-zero_time = datetime(1900, 1, 1, 0, 0, 0).time()
-rawData.insert(0, 'class', "NONE")
-rawData.insert(1, 'Time_In_PST', zero_time)
+# zero_time = datetime(1900, 1, 1, 0, 0, 0).time()
+# rawData.insert(0, 'class', "NONE")
+# rawData.insert(1, 'Time_In_PST', zero_time)
 
-for row in rawData.itertuples():
-    rawData.at[row.Index, 'Time_In_PST'] = convert_timestamp_to_pacific(getattr(row, 'time'))
-    for schedRow in scheduleData.itertuples():
-        timeA = convert_string_to_time(getattr(schedRow, 'TimeStart'))
-        timeB = convert_string_to_time(getattr(schedRow, 'TimeEnd'))
-        if  timeA < rawData.at[row.Index, 'Time_In_PST'] <= timeB:
-            rawData.at[row.Index, 'class'] = getattr(schedRow, 'Class')
-            break
-rawData = rawData[rawData['class'] != 'DELETE']
-print(rawData.head)
+pNum = input("Enter the participant number: ")
 
-rawData.to_csv(saveLocation, index=False)
+rawData = pd.read_csv("/Users/tommoore/Documents/GitHub/Research/P0" + pNum + "/OuraRing/HeartRate/P001OrHrRAW.csv")
+
+if pNum == "04" OR pNum == "05":
+    scheduleDataFri = pd.read_csv("/Users/tommoore/Documents/GitHub/Research/Schedules/schedData_P(04,05)_Fr.csv")
+    scdhuleDataOth = pd.read_csv("/Users/tommoore/Documents/GitHub/Research/Schedules/schedData_P(04,05)_M-Th.csv")
+else:
+    scheduleDataFri = pd.read_csv("/Users/tommoore/Documents/GitHub/Research/Schedules/schedData_P(01,02,03,06,07,08,09,12,14,16)_FR.csv")
+    scdhuleDataOth = pd.read_csv("/Users/tommoore/Documents/GitHub/Research/Schedules/schedData_P(01,02,03,06,07,08,09,12,14,16)_M-TH.csv")
+
+dfList = []
