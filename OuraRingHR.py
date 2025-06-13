6e1966a5b21a6a442c632ff08a210f7c0c0d5b60
Tom Moore <thommoore@chapman.edu>
2025-06-12 17:41:15 -0700

    Fixing edits on wrong file

--- OuraRingHR.py
+++ OuraRingHR.py
@@ -20,14 +20,24 @@ def convert_iso_to_pacific_date(timestamp):
     dt_pacific = dt_utc.astimezone(pacific_tz)
     return dt_pacific.date()
 
-rawDataPath = input("Enter the file path of the raw data: ")
-rawData = pd.read_csv(rawDataPath)
+pNum = input("Enter the participant number: ")
 
-scheduleDataPath = input("Enter the file path of the schedule data: ")
-scheduleData = pd.read_csv(scheduleDataPath)
+rawData = pd.read_csv("/Users/tommoore/Documents/GitHub/Research/P0" + pNum + "/OuraRing/HeartRate/P001OrHrRAW.csv")
+
+if pNum == "04" OR pNum == "05":
+    scheduleDataFri = pd.read_csv("/Users/tommoore/Documents/GitHub/Research/Schedules/schedData_P(04,05)_Fr.csv")
+    scdhuleDataOth = pd.read_csv("/Users/tommoore/Documents/GitHub/Research/Schedules/schedData_P(04,05)_M-Th.csv")
+else:
+    scheduleDataFri = pd.read_csv("/Users/tommoore/Documents/GitHub/Research/Schedules/schedData_P(01,02,03,06,07,08,09,12,14,16)_FR.csv")
+    scdhuleDataOth = pd.read_csv("/Users/tommoore/Documents/GitHub/Research/Schedules/schedData_P(01,02,03,06,07,08,09,12,14,16)_M-TH.csv")
 
 zero_time = datetime(1900, 1, 1, 0, 0, 0).time()
 rawData.insert(0, 'class', "NONE")
 rawData.insert(1, 'Time_In_PST', zero_time)
 
-nextDate = datetime(1900, 1, 1, 0, 0, 0).date()
+dfList = []
+
+# create different data frames
+# Add Unix and PST time
+# label with schedule data
+# Save to file
