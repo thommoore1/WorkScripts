af617e93ec9f82abae041a3e2ec6f7f80a81d56b
Tom Moore <thommoore@chapman.edu>
2025-06-26 16:05:20 -0700

    Removing unused columns

--- HealthApp.py
+++ HealthApp.py
@@ -1,4 +1,16 @@
 import pandas as pd
+import pytz
+import os
 
-df = pd.read_xml("/Users/tommoore/Documents/GitHub/Research/P001/HealthApp/Raw/P001 export.xml")
-df.to_csv("/Users/tommoore/Documents/GitHub/Research/P001/HealthApp/Labeled/LabeledTest.csv", index=False)
+# Prompt for participant number
+pNum = input("Enter the participant number: ")
+
+#Gathering parent paths
+rawParentPath = f"/Users/tommoore/Documents/GitHub/Research/P0{pNum}/HealthApp/Raw/export.csv"
+labeledActivityParentPath = f"/Users/tommoore/Documents/GitHub/Research/P0{pNum}/HealthApp/Labeled/ActivitySummary"
+labeledRecordParentPath = f"/Users/tommoore/Documents/GitHub/Research/P0{pNum}/HealthApp/Labeled/ActivitySummary"
+
+majorDF = pd.read_csv(rawParentPath)
+majorDF.drop(columns=["/@locale"], inplace=True)
+majorDF.drop(columns=[col for col in majorDF.columns if col.startswith("/Me/")], inplace=True)
+majorDF.drop(columns=[col for col in majorDF.columns if col.startswith("/Workout/")], inplace=True)
