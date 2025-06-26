82a3ed93ba4171b0682d1c93e946b0f4d4c7de80
Tom Moore <thommoore@chapman.edu>
2025-06-26 16:05:43 -0700

    Didn't fully dave

--- HealthApp.py
+++ HealthApp.py
@@ -1,16 +1,27 @@
 import pandas as pd
 import pytz
 import os
+from collections import defaultdict
+
+
+
+
+
+
 
 # Prompt for participant number
 pNum = input("Enter the participant number: ")
 
-#Gathering parent paths
+# Gathering parent paths
 rawParentPath = f"/Users/tommoore/Documents/GitHub/Research/P0{pNum}/HealthApp/Raw/export.csv"
 labeledActivityParentPath = f"/Users/tommoore/Documents/GitHub/Research/P0{pNum}/HealthApp/Labeled/ActivitySummary"
 labeledRecordParentPath = f"/Users/tommoore/Documents/GitHub/Research/P0{pNum}/HealthApp/Labeled/ActivitySummary"
 
-majorDF = pd.read_csv(rawParentPath)
+# Make dataframe of entire csv (will split later)
+majorDF = pd.read_csv(rawParentPath, low_memory=False)
+
+# Drop uneccessary colummns
 majorDF.drop(columns=["/@locale"], inplace=True)
 majorDF.drop(columns=[col for col in majorDF.columns if col.startswith("/Me/")], inplace=True)
 majorDF.drop(columns=[col for col in majorDF.columns if col.startswith("/Workout/")], inplace=True)
+
